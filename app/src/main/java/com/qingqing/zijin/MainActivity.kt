430f54ce0cf4d6680a89a5f6f0c03db52389e226
CamusJiang <camus2026@qq.com>
2026-09-15 11:41:03 +0800

    fix: 修复AudioAttributes/AudioFocusRequest import路径错误

--- app/src/main/java/com/qingqing/zijin/MainActivity.kt
+++ app/src/main/java/com/qingqing/zijin/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.qingqing.zijin
 
 import android.annotation.SuppressLint
-import android.content.AudioAttributes
-import android.content.AudioFocusRequest
-import android.content.AudioManager
+import android.media.AudioAttributes
+import android.media.AudioFocusRequest
+import android.media.AudioManager
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
